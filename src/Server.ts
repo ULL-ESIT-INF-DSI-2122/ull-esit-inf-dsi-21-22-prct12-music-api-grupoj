3a2670382c54cf6b27ff559d92dd61ce9b7c2295
Alejandro Perez <alu0101215310@ull.edu.es>
2022-05-21 23:58:24 +0000

    init

--- src/Server.ts
+++ src/Server.ts
@@ -12,12 +12,12 @@ export class Server {
     mongoose.set('useFindAndModify', true);
     mongoose.connect(process.env.MONGODB_URL || DATABASE, {
       useNewUrlParser: true,
-      useCreateIndex: true
-    }).then(db => console.log("Database connected!"))
-    .catch(db => console.error("Error connecting to  te database"));
+      useCreateIndex: true,
+    }).then((db) => console.log('Database connected!'))
+        .catch((db) => console.error('Error connecting to  te database'));
 
-    this.app.set('port', process.env.PORT  || port);
-    
+    this.app.set('port', process.env.PORT || port);
+
     this.app.use(morgan('dev'));
     this.app.use(express.json());
     this.app.use(express.urlencoded({extended: false}));
@@ -29,7 +29,7 @@ export class Server {
   public start() {
     this.app.listen(this.app.get('port'), () => {
       console.log('Server listening on port', this.app.get('port'));
-    })
+    });
   }
 }
 
